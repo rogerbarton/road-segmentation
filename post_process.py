2cb6f5bae32c8518f100470d28afc7e84e9ed12d
Guy Lüthi <guluethi@student.ethz.ch>
2021-07-12 10:21:39 +0200

    fixed post processing

--- post_process.py
+++ post_process.py
@@ -2,7 +2,18 @@ from train import UNet
 import torch
 import pydensecrf.densecrf as dcrf
 import pydensecrf.utils as utils
+import argparse
+from glob import glob
+import numpy as np
+import cv2
+from train import load_all_from_path, np_to_tensor
+import re
 
+
+# some constants
+PATCH_SIZE = 16  # pixels per side of square patches
+VAL_SIZE = 10  # size of the validation set (number of images)
+CUTOFF = 0.25  # minimum average brightness for a mask patch to be classified as containing road
 
 def morphological_postprocessing(imgs):
     out = []
@@ -14,9 +25,29 @@ def morphological_postprocessing(imgs):
         out.append(img)
     return out
 
+def post_processing(outs):
+    return outs
+
+def create_submission(test_pred, test_filenames, submission_filename):
+    test_path='test_images/test_images'
+    with open(submission_filename, 'w') as f:
+        f.write('id,prediction\n')
+        for fn, patch_array in zip(sorted(test_filenames), test_pred):
+            img_number = int(re.search(r"\d+", fn).group(0))
+            for i in range(patch_array.shape[0]):
+                for j in range(patch_array.shape[1]):
+                    f.write("{:03d}_{}_{},{}\n".format(img_number, j*PATCH_SIZE, i*PATCH_SIZE, int(patch_array[i, j])))
+
 def main():
-    model = UNet()
-    model.load_state_dict(torch.load("model.pth"))
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('model', metavar='model', type=str, nargs=1, help='the model')
+    args = parser.parse_args()
+
+    print(args.model)
+    model = UNet().to(device)
+    model.load_state_dict(torch.load(args.model[0]))
     model.eval()
 
     # predict on test set
@@ -29,18 +60,7 @@ def main():
     test_images = np.stack([cv2.resize(img, dsize=(384, 384)) for img in test_images], 0)
     test_images = np_to_tensor(np.moveaxis(test_images, -1, 1), device)
 
-    test_pred = []
-
-    for t in test_images.unsqueeze(1):
-        y_hat = model(t).detach().cpu().numpy()
-        crf_output = np.zeros(y_hat.shape)
-        images = t.data.cpu().numpy().astype(np.uint8)
-        for i, (image, prob_map) in enumerate(zip(images, y_hat)):
-            image = image.transpose(1, 2, 0)
-            crf_output[i] = dense_crf(image, prob_map)
-        y_hat = crf_output
-
-        test_pred.append(y_hat)
+    test_pred = [model(t).detach().cpu().numpy() for t in test_images.unsqueeze(1)]
 
 
     test_pred = np.concatenate(test_pred, 0)
