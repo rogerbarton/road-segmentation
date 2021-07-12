1a8750b16d8832f081e53eee362a7c6df771a81d
Lukas Gygi <lukas.gygi@gmail.com>
2021-07-12 14:52:25 +0200

    random noise in loader

--- dataset.py
+++ dataset.py
@@ -43,8 +43,20 @@ class RoadDataset(Dataset):
     def __len__(self):
         return len(self.img_list)
 
-    def _augment(self, image, mask):
-        # TODO: maybe noise (salt&pepper, gaussian) and colorshift
+    # do some noise augmentation, on np arrays
+    def _augment(self, image, mask, sigma=0.1, snp_prob=0.05):
+        # TODO: maybe colorshift
+        # gaussian noise:
+        # image = (image + (0.01**0.5) * torch.randn(image.size())).clamp_(0, 1)
+        image = (
+            (image + np.random.normal(0.0, sigma, image.shape))
+            .clip(0.0, 1.0)
+            .astype(np.float32)
+        )
+        # salt and pepper noise:
+        rnd = np.random.rand(*image.shape)
+        image[rnd < snp_prob / 2] = 0.0
+        image[rnd > 1 - snp_prob / 2] = 1.0
         return image, mask
 
     def __getitem__(self, idx):
@@ -54,11 +66,18 @@ class RoadDataset(Dataset):
             image_np = cv2.resize(image_np, dsize=self.resize_to)
             mask_np = cv2.resize(mask_np, dsize=self.resize_to)
         image_np = np.moveaxis(image_np, -1, 0)  # pytorch needs CHW instead of HWC
-        mask_np = np.expand_dims(mask_np, axis=0) # needs to be 1HW
+        mask_np = np.expand_dims(mask_np, axis=0)  # needs to be 1HW
+        if self.transforms:
+            image_np = self.transforms(image_np)
+        if self.augment:
+            image_np, mask_np = self._augment(image_np, mask_np)
         image = np_to_tensor(image_np, self.device)
         mask = np_to_tensor(mask_np, self.device)
-        if self.transforms:
-            image = self.transforms(image)
-        if self.augment:
-            image, mask = self._augment(image, mask)
+
+        # for visualizing:
+        # _, axarr = plt.subplots(1, 2)
+        # axarr[0].imshow(image.permute(1, 2, 0))
+        # axarr[1].imshow(mask[0])
+        # plt.show()
+
         return image, mask
