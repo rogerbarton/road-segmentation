2ae14c9e8573142f1a371c8b72e70d2ca72e69c1
Guy Lüthi <guluethi@student.ethz.ch>
2021-07-14 12:19:20 +0200

    made crf work

--- post_process.py
+++ post_process.py
@@ -8,6 +8,11 @@ import numpy as np
 import cv2
 from train import load_all_from_path, np_to_tensor
 import re
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import pydensecrf.densecrf as dcrf
+import pydensecrf.utils as utils
 
 
 # some constants
@@ -38,6 +43,38 @@ def create_submission(test_pred, test_filenames, submission_filename):
                 for j in range(patch_array.shape[1]):
                     f.write("{:03d}_{}_{},{}\n".format(img_number, j*PATCH_SIZE, i*PATCH_SIZE, int(patch_array[i, j])))
 
+
+class DenseCRF(object):
+    def __init__(self, iter_max, pos_w, pos_xy_std, bi_w, bi_xy_std, bi_rgb_std):
+        self.iter_max = iter_max
+        self.pos_w = pos_w
+        self.pos_xy_std = pos_xy_std
+        self.bi_w = bi_w
+        self.bi_xy_std = bi_xy_std
+        self.bi_rgb_std = bi_rgb_std
+
+    def __call__(self, image, probmap):
+        C, H, W = probmap.shape
+
+        U = utils.unary_from_softmax(probmap)
+        U = np.ascontiguousarray(U)
+
+        image = np.ascontiguousarray(image)
+
+        d = dcrf.DenseCRF2D(W, H, C)
+        d.setUnaryEnergy(U)
+        #d.addPairwiseGaussian(sxy=self.pos_xy_std, compat=self.pos_w)
+        d.addPairwiseGaussian(sxy=(3,3), compat=3, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+        #d.addPairwiseBilateral(
+        #    sxy=self.bi_xy_std, srgb=self.bi_rgb_std, rgbim=image, compat=self.bi_w
+        #)
+        d.addPairwiseBilateral(sxy=(80,80), srgb=(13,13,13), rgbim=image, compat=10, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+
+        Q = d.inference(self.iter_max)
+        Q = np.array(Q).reshape((C, H, W))
+
+        return Q
+
 def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     parser = argparse.ArgumentParser()
@@ -54,6 +91,7 @@ def main():
     test_path = 'test'
     test_filenames = (glob(test_path + '/*.png'))
     test_images = load_all_from_path(test_path)
+    #test_images = np.array([test_images[0], test_images[1]])
     # batch_size = test_images.shape[0]
     size = test_images.shape[1:3]
     # we also need to resize the test images. This might not be the best ideas depending on their spatial resolution.
@@ -66,12 +104,45 @@ def main():
     test_pred = np.concatenate(test_pred, 0)
     test_pred = np.moveaxis(test_pred, 1, -1)  # CHW to HWC
     test_pred = np.stack([cv2.resize(img, dsize=size) for img in test_pred], 0)  # resize to original shape
+
+    post_processor = DenseCRF(
+        iter_max=10,    # 10
+        pos_xy_std=3,   # 3
+        pos_w=3,        # 3
+        bi_xy_std=140,  # 121, 140
+        bi_rgb_std=5,   # 5, 5
+        bi_w=5,         # 4, 5
+    )
+
+    raw_images = np.stack([np.array(Image.open(f)) for f in sorted(glob(test_path + '/*.png'))])
+
+    inv_func = np.vectorize(lambda x : 1 - x)
+
+    output = []
+
+    for i, pred in enumerate(test_pred):
+        print("Processing image " + str(i))
+        raw_img = raw_images[i]
+        raw_img = raw_img.astype(np.uint8)
+        plt.imsave('raw.jpeg', raw_img)
+        plt.imsave('first.jpeg', pred)
+        plt.imsave('sec.jpeg', inv_func(pred))
+        probs_labels = np.array([inv_func(pred), pred])
+        prob = post_processor(raw_img, probs_labels)
+        output.append(np.argmax(prob, axis=0).astype(np.uint8))
+        plt.imsave('post_proc.jpeg', output[i])
+
+    test_pred = np.array(output)
+
     
+
+    """
     # morphological postprocessing
     
     kernel = np.ones((3, 3), np.uint8)
     test_pred = np.stack([cv2.erode(img, kernel, iterations=7) for img in test_pred], 0)
     test_pred = np.stack([cv2.dilate(img, kernel, iterations=7) for img in test_pred], 0)
+    """
     
     # now compute labels
     test_pred = test_pred.reshape((-1, size[0] // PATCH_SIZE, PATCH_SIZE, size[0] // PATCH_SIZE, PATCH_SIZE))
