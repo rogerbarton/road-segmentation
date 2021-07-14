feef3669077bf65dde9be4fca5a2f9608d103e71
Lukas Gygi <lukas.gygi@gmail.com>
2021-07-14 16:31:39 +0200

    color augmentation

--- dataset.py
+++ dataset.py
@@ -30,7 +30,7 @@ class RoadDataset(Dataset):
         device,
         resize_to=(400, 400),
         transforms=None,
-        augment=True,
+        augment=False,
     ):
         self.img_dir = img_dir
         self.transforms = transforms
@@ -44,8 +44,8 @@ class RoadDataset(Dataset):
         return len(self.img_list)
 
     # do some noise augmentation, on np arrays
-    def _augment(self, image, mask, sigma=0.1, snp_prob=0.05):
-        # TODO: maybe colorshift
+    def _augment_noise(self, image, mask, sigma=0.1, snp_prob=0.02):
+        # TODO: contrast and brightness adjust
         # gaussian noise:
         # image = (image + (0.01**0.5) * torch.randn(image.size())).clamp_(0, 1)
         image = (
@@ -59,6 +59,17 @@ class RoadDataset(Dataset):
         image[rnd > 1 - snp_prob / 2] = 1.0
         return image, mask
 
+    # need tensor or PIL image
+    def _augment_brightness_contrast(self, image, mask):
+        brightness = np.random.uniform(0.5, 1.5)
+        contrast = np.random.uniform(0.5, 1.5)
+        hue = np.random.uniform(-0.2, 0.2)
+
+        image = F.adjust_brightness(image, brightness)
+        image = F.adjust_contrast(image, contrast)
+        image = F.adjust_hue(image, hue)
+        return image, mask
+
     def __getitem__(self, idx):
         image_np = np.array(Image.open(self.img_list[idx])).astype(np.float32) / 255
         mask_np = np.array(Image.open(self.mask_list[idx])).astype(np.float32) / 255
@@ -67,17 +78,23 @@ class RoadDataset(Dataset):
             mask_np = cv2.resize(mask_np, dsize=self.resize_to)
         image_np = np.moveaxis(image_np, -1, 0)  # pytorch needs CHW instead of HWC
         mask_np = np.expand_dims(mask_np, axis=0)  # needs to be 1HW
+        if self.augment:
+            image, mask = self._augment_brightness_contrast(
+                torch.from_numpy(image_np), torch.from_numpy(mask_np)
+            )
+            image_np = image.numpy()
+            mask_np = mask.numpy()
         if self.transforms:
             image_np = self.transforms(image_np)
         if self.augment:
-            image_np, mask_np = self._augment(image_np, mask_np)
+            image_np, mask_np = self._augment_noise(image_np, mask_np)
         image = np_to_tensor(image_np, self.device)
         mask = np_to_tensor(mask_np, self.device)
 
         # for visualizing:
-        # _, axarr = plt.subplots(1, 2)
-        # axarr[0].imshow(image.permute(1, 2, 0))
-        # axarr[1].imshow(mask[0])
-        # plt.show()
+        _, axarr = plt.subplots(1, 2)
+        axarr[0].imshow(image.permute(1, 2, 0))
+        axarr[1].imshow(mask[0])
+        plt.show()
 
         return image, mask
