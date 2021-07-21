1f60c43b6e52de63c68444ecc198b8c50b26fa37
Guy Lüthi <guluethi@student.ethz.ch>
2021-07-21 10:47:41 +0200

    better train.py

---
+++ models/baselines.py
@@ -0,0 +1,186 @@
+import torch
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+from tqdm.notebook import tqdm
+import os
+import numpy as np
+from glob import glob
+from PIL import Image
+
+
+# some constants
+PATCH_SIZE = 16  # pixels per side of square patches
+VAL_SIZE = 10  # size of the validation set (number of images)
+CUTOFF = 0.25  # minimum average brightness for a mask patch to be classified as containing road
+
+def load_all_from_path(path):
+    # loads all HxW .pngs contained in path as a 4D np.array of shape (n_images, H, W, 3)
+    # images are loaded as floats with values in the interval [0., 1.]
+    return np.stack([np.array(Image.open(f)) for f in sorted(glob(path + '/*.png'))]).astype(np.float32) / 255.
+
+def np_to_tensor(x, device):
+    # allocates tensors from np.arrays
+    if device == 'cpu':
+        return torch.from_numpy(x).cpu()
+    else:
+        return torch.from_numpy(x).contiguous().pin_memory().to(device=device, non_blocking=True)
+
+def image_to_patches(images, masks=None):
+    # takes in a 4D np.array containing images and (optionally) a 4D np.array containing the segmentation masks
+    # returns a 4D np.array with an ordered sequence of patches extracted from the image and (optionally) a np.array containing labels
+    n_images = images.shape[0]  # number of images
+    h, w = images.shape[1:3]  # shape of images
+    assert (h % PATCH_SIZE) + (w % PATCH_SIZE) == 0  # make sure images can be patched exactly
+
+    h_patches = h // PATCH_SIZE
+    w_patches = w // PATCH_SIZE
+    patches = images.reshape((n_images, h_patches, PATCH_SIZE, h_patches, PATCH_SIZE, -1))
+    patches = np.moveaxis(patches, 2, 3)
+    patches = patches.reshape(-1, PATCH_SIZE, PATCH_SIZE, 3)
+    if masks is None:
+        return patches
+
+    masks = masks.reshape((n_images, h_patches, PATCH_SIZE, h_patches, PATCH_SIZE, -1))
+    masks = np.moveaxis(masks, 2, 3)
+    labels = np.mean(masks, (-1, -2, -3)) > CUTOFF  # compute labels
+    labels = labels.reshape(-1).astype(np.float32)
+    return patches, labels
+
+class ImageDataset(torch.utils.data.Dataset):
+    # dataset class that deals with loading the data and making it available by index.
+
+    def __init__(self, path, device, use_patches=True, resize_to=(400, 400)):
+        self.path = path
+        self.device = device
+        self.use_patches = use_patches
+        self.resize_to=resize_to
+        self.x, self.y, self.n_samples = None, None, None
+        self._load_data()
+
+    def _load_data(self):  # not very scalable, but good enough for now
+        self.x = load_all_from_path(os.path.join(self.path, 'images'))
+        self.y = load_all_from_path(os.path.join(self.path, 'groundtruth'))
+        if self.use_patches:  # split each image into patches
+            self.x, self.y = image_to_patches(self.x, self.y)
+        elif self.resize_to != (self.x.shape[1], self.x.shape[2]):  # resize images
+            self.x = np.stack([cv2.resize(img, dsize=self.resize_to) for img in self.x], 0)
+            self.y = np.stack([cv2.resize(mask, dsize=self.resize_to) for mask in self.y], 0)
+        self.x = np.moveaxis(self.x, -1, 1)  # pytorch works with CHW format instead of HWC
+        self.n_samples = len(self.x)
+
+    def _preprocess(self, x, y):
+        # to keep things simple we will not apply transformations to each sample,
+        # but it would be a very good idea to look into preprocessing
+        return x, y
+
+    def __getitem__(self, item):
+        return self._preprocess(np_to_tensor(self.x[item], self.device), np_to_tensor(self.y[[item]], self.device))
+    
+    def __len__(self):
+        return self.n_samples
+
+
+def show_val_samples(x, y, y_hat, segmentation=False):
+    # training callback to show predictions on validation set
+    imgs_to_draw = min(5, len(x))
+    if x.shape[-2:] == y.shape[-2:]:  # segmentation
+        fig, axs = plt.subplots(3, imgs_to_draw, figsize=(18.5, 12))
+        for i in range(imgs_to_draw):
+            axs[0, i].imshow(np.moveaxis(x[i], 0, -1))
+            axs[1, i].imshow(np.concatenate([np.moveaxis(y_hat[i], 0, -1)] * 3, -1))
+            axs[2, i].imshow(np.concatenate([np.moveaxis(y[i], 0, -1)]*3, -1))
+            axs[0, i].set_title(f'Sample {i}')
+            axs[1, i].set_title(f'Predicted {i}')
+            axs[2, i].set_title(f'True {i}')
+            axs[0, i].set_axis_off()
+            axs[1, i].set_axis_off()
+            axs[2, i].set_axis_off()
+    else:  # classification
+        fig, axs = plt.subplots(1, imgs_to_draw, figsize=(18.5, 6))
+        for i in range(imgs_to_draw):
+            axs[i].imshow(np.moveaxis(x[i], 0, -1))
+            axs[i].set_title(f'True: {np.round(y[i]).item()}; Predicted: {np.round(y_hat[i]).item()}')
+            axs[i].set_axis_off()
+    plt.show()
+
+class PatchCNN(nn.Module):
+    # simple CNN for classification of patches
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
+                                 nn.ReLU(),
+                                 nn.MaxPool2d(2, 2),
+                                 nn.BatchNorm2d(16),
+                                 nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+                                 nn.ReLU(),
+                                 nn.MaxPool2d(2, 2),
+                                 nn.BatchNorm2d(32),
+                                 nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+                                 nn.ReLU(),
+                                 nn.MaxPool2d(2, 2),
+                                 nn.BatchNorm2d(64),
+                                 nn.Dropout(0.5),
+                                 nn.Flatten(),
+                                 nn.Linear(256, 10),
+                                 nn.ReLU(),
+                                 nn.Dropout(0.5),
+                                 nn.Linear(10, 1),
+                                 nn.Sigmoid())
+
+    def forward(self, x):
+        return self.net(x)
+
+def accuracy_fn(y_hat, y):
+    # computes classification accuracy
+    return (y_hat.round() == y.round()).float().mean()
+
+
+class Block(nn.Module):
+    # a repeating structure composed of two convolutional layers with batch normalization and ReLU activations
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.block = nn.Sequential(nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3, padding=1),
+                                   nn.ReLU(),
+                                   nn.BatchNorm2d(out_ch),
+                                   nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3, padding=1),
+                                   nn.ReLU())
+
+    def forward(self, x):
+        return self.block(x)
+
+        
+class UNet(nn.Module):
+    # UNet-like architecture for single class semantic segmentation.
+    def __init__(self, chs=(3,64,128,256,512,1024)):
+        super().__init__()
+        enc_chs = chs  # number of channels in the encoder
+        dec_chs = chs[::-1][:-1]  # number of channels in the decoder
+        self.enc_blocks = nn.ModuleList([Block(in_ch, out_ch) for in_ch, out_ch in zip(enc_chs[:-1], enc_chs[1:])])  # encoder blocks
+        self.pool = nn.MaxPool2d(2)  # pooling layer (can be reused as it will not be trained)
+        self.upconvs = nn.ModuleList([nn.ConvTranspose2d(in_ch, out_ch, 2, 2) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])])  # deconvolution
+        self.dec_blocks = nn.ModuleList([Block(in_ch, out_ch) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])])  # decoder blocks
+        self.head = nn.Sequential(nn.Conv2d(dec_chs[-1], 1, 1), nn.Sigmoid()) # 1x1 convolution for producing the output
+
+    def forward(self, x):
+        # encode
+        enc_features = []
+        for block in self.enc_blocks[:-1]:
+            x = block(x)  # pass through the block
+            enc_features.append(x)  # save features for skip connections
+            x = self.pool(x)  # decrease resolution
+        x = self.enc_blocks[-1](x)
+        # decode
+        for block, upconv, feature in zip(self.dec_blocks, self.upconvs, enc_features[::-1]):
+            x = upconv(x)  # increase resolution
+            x = torch.cat([x, feature], dim=1)  # concatenate skip features
+            x = block(x)  # pass through the block
+        return self.head(x)  # reduce to 1 channel
+
+
+def patch_accuracy_fn(y_hat, y):
+    # computes accuracy weighted by patches (metric used on Kaggle for evaluation)
+    h_patches = y.shape[-2] // PATCH_SIZE
+    w_patches = y.shape[-1] // PATCH_SIZE
+    patches_hat = y_hat.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean((-1, -3)) > CUTOFF
+    patches = y.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean((-1, -3)) > CUTOFF
+    return (patches == patches_hat).float().mean()
--- train.py
+++ train.py
@@ -1,15 +1,9 @@
 # some basic imports
 
-import math
 import os
 import re
-import cv2
 import random
-import sys
 import numpy as np
-import matplotlib.pyplot as plt
-from glob import glob
-from random import sample
 from PIL import Image
 import torchvision.transforms as TF
 import torchvision.transforms.functional as F
@@ -20,6 +14,10 @@ from tqdm.notebook import tqdm
 from dataset import RoadDataset
 import datetime
 import traceback
+import argparse
+
+from models.u_net import UNet as newnet
+from models.baselines import ImageDataset, UNet, PatchCNN
 
 # some constants
 PATCH_SIZE = 16  # pixels per side of square patches
@@ -29,68 +27,7 @@ CUTOFF = 0.25  # minimum average brightness for a mask patch to be classified as
 def count_parameters(net):
     """Count number of trainable parameters in `net`."""
     return sum(p.numel() for p in net.parameters() if p.requires_grad)
-
-
-def preprocessing():
-    for img in glob("training/images/*.png"):
-        rnd = random.random()
-        image = Image.open(img)
-        mask = img.replace('images', 'groundtruth')
-        mask2 = Image.open(mask)
-        img2 = img[:-4]
-        if rnd <= 0.5:
-            image2 = F.hflip(image)
-            mask3 = F.hflip(mask2)
-            img2 = img2 + 'hflip' + '.png'
-        else:
-            image2 = F.vflip(image)
-            mask3 = F.hflip(mask2)
-            img2 = img2 + 'vflip' + '.png'
-            image2.save(img2)
-            maskstr = img2.replace('images', 'groundtruth')
-            mask3.save(maskstr)
-        for i in range(8):
-            rnd = random.random()
-            if rnd < 0.5:
-                angle = random.randint(45*i, 45*(i+1))
-                image2 = F.rotate(image, angle)
-                mask3 = F.rotate(mask2, angle)
-                img2 = img[:-4]
-                img2 = img2 + '_rot' + str(angle) + '.png'
-                image2.save(img2)
-                maskstr = img2.replace('images', 'groundtruth')
-                mask3.save(maskstr)
-
-def load_all_from_path(path):
-    # loads all HxW .pngs contained in path as a 4D np.array of shape (n_images, H, W, 3)
-    # images are loaded as floats with values in the interval [0., 1.]
-    return np.stack([np.array(Image.open(f)) for f in sorted(glob(path + '/*.png'))]).astype(np.float32) / 255.
-
-def load_all_from_path_resize(path, resize_to=(400, 400)):
-    return np.stack([cv2.resize(np.array(Image.open(f)), dsize=resize_to) for f in sorted(glob(path + '/*.png'))]).astype(np.float32) / 255.
-
         
-
-def image_to_patches(images, masks=None):
-    # takes in a 4D np.array containing images and (optionally) a 4D np.array containing the segmentation masks
-    # returns a 4D np.array with an ordered sequence of patches extracted from the image and (optionally) a np.array containing labels
-    n_images = images.shape[0]  # number of images
-    h, w = images.shape[1:3]  # shape of images
-    assert (h % PATCH_SIZE) + (w % PATCH_SIZE) == 0  # make sure images can be patched exactly
-
-    h_patches = h // PATCH_SIZE
-    w_patches = w // PATCH_SIZE
-    patches = images.reshape((n_images, h_patches, PATCH_SIZE, h_patches, PATCH_SIZE, -1))
-    patches = np.moveaxis(patches, 2, 3)
-    patches = patches.reshape(-1, PATCH_SIZE, PATCH_SIZE, 3)
-    if masks is None:
-        return patches
-
-    masks = masks.reshape((n_images, h_patches, PATCH_SIZE, h_patches, PATCH_SIZE, -1))
-    masks = np.moveaxis(masks, 2, 3)
-    labels = np.mean(masks, (-1, -2, -3)) > CUTOFF  # compute labels
-    labels = labels.reshape(-1).astype(np.float32)
-    return patches, labels
 
 def np_to_tensor(x, device):
     # allocates tensors from np.arrays
@@ -161,90 +98,6 @@ def train(train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimiz
     print('Finished Training')
 
 
-class Block(nn.Module):
-    # a repeating structure composed of two convolutional layers with batch normalization and ReLU activations
-    def __init__(self, in_ch, out_ch):
-        super().__init__()
-        self.block = nn.Sequential(nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3, padding=1),
-                                   nn.BatchNorm2d(out_ch),
-                                   nn.ReLU(),
-                                   nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3, padding=1),
-                                   nn.BatchNorm2d(out_ch), # changed batchnorm to be before activation
-                                   nn.ReLU())
-
-    def forward(self, x):
-        return self.block(x)
-
-
-class ImageDataset(torch.utils.data.Dataset):
-    # dataset class that deals with loading the data and making it available by index.
-
-    def __init__(self, path, device, use_patches=True, resize_to=(400, 400)):
-        self.path = path
-        self.device = device
-        self.use_patches = use_patches
-        self.resize_to = resize_to
-        self.x, self.y, self.n_samples = None, None, None
-        self._load_data()
-
-    def _load_data(self):  # not very scalable, but good enough for now
-        self.x = load_all_from_path(os.path.join(self.path, 'images'))
-        self.y = load_all_from_path(os.path.join(self.path, 'groundtruth'))
-        if self.use_patches:  # split each image into patches
-            self.x, self.y = image_to_patches(self.x, self.y)
-        elif self.resize_to != (self.x.shape[1], self.x.shape[2]):  # resize images
-            self.x = np.stack([cv2.resize(img, dsize=self.resize_to) for img in self.x], 0)
-            self.y = np.stack([cv2.resize(mask, dsize=self.resize_to) for mask in self.y], 0)
-        self.x = np.moveaxis(self.x, -1, 1)  # pytorch works with CHW format instead of HWC
-        self.n_samples = len(self.x)
-
-    def _preprocess(self, x, y):
-        # to keep things simple we will not apply transformations to each sample,
-        # but it would be a very good idea to look into preprocessing
-        return x, y
-
-    def __getitem__(self, item):
-        return self._preprocess(np_to_tensor(self.x[item], self.device), np_to_tensor(self.y[[item]], self.device))
-    
-    def __len__(self):
-        return self.n_samples
-
-        
-class UNet(nn.Module):
-    # UNet-like architecture for single class semantic segmentation.
-    def __init__(self, chs=(3,16,32,64,128,256,512,1024)): # added 16 and 32
-        super().__init__()
-        enc_chs = chs  # number of channels in the encoder
-        dec_chs = chs[::-1][:-1]  # number of channels in the decoder
-        self.enc_blocks = nn.ModuleList([Block(in_ch, out_ch) for in_ch, out_ch in zip(enc_chs[:-1], enc_chs[1:])])  # encoder blocks
-        self.pool = nn.MaxPool2d(2)  # pooling layer (can be reused as it will not be trained)
-        self.upconvs = nn.ModuleList([nn.ConvTranspose2d(in_ch, out_ch, 2, 2) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])])  # deconvolution
-        self.dec_blocks = nn.ModuleList([Block(in_ch, out_ch) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])])  # decoder blocks
-        self.head = nn.Sequential(nn.Conv2d(dec_chs[-1], 1, 1), nn.Sigmoid()) # 1x1 convolution for producing the output
-
-        # self.gcn = GCN(384, 384, 0.3, num_stage=0, node_n=384)
-
-    def forward(self, x):
-        # encode
-        enc_features = []
-        for block in self.enc_blocks[:-1]:
-            x = block(x)  # pass through the block
-            enc_features.append(x)  # save features for skip connections
-            x = self.pool(x)  # decrease resolution
-        x = self.enc_blocks[-1](x)
-        # decode
-        for block, upconv, feature in zip(self.dec_blocks, self.upconvs, enc_features[::-1]):
-            x = upconv(x)  # increase resolution
-            x = torch.cat([x, feature], dim=1)  # concatenate skip features
-            x = block(x)  # pass through the block
-
-        return self.head(x) #.squeeze(dim=1)  # reduce to 1 channel,
-        
-        # retval = self.gcn(pre_postproc).unsqueeze(dim=1)
-
-        # return retval
-
-
 def patch_accuracy_fn(y_hat, y):
     # computes accuracy weighted by patches (metric used on Kaggle for evaluation)
     h_patches = y.shape[-2] // PATCH_SIZE
@@ -259,24 +112,105 @@ def accuracy_fn(y_hat, y):
 
 
 def main():
-    random.seed(17)
+    # This is the main training part
+    # Here we fix the seeds, call the training loop and Save the model
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model",
+        help="which model to train, choose from [cnn, unet, new_unet]",
+        default="new_unet",
+    )
+    parser.add_argument(
+        "--kernel_size",
+        help="The kernel size to use, choose from [3, 5, 7]",
+        default=3, type=int
+    )
+    parser.add_argument(
+        "--pre_processing",
+        help="What preprocessing technique to use, choose from [otf, none, altered_images]",
+        default="none"
+    )
+    parser.add_argument(
+        "--loss",
+        help="What loss to use, choose from [BCE, BCE_with_logit]",
+        default="BCE"
+    )
+    parser.add_argument(
+        "--lr",
+        help="What learningrate to use",
+        default=0.001, type=float
+    )
+    parser.add_argument(
+        "--optimizer",
+        help="What optimizer to use, choose from [adam, SGD, adamax]",
+        default="adam"
+    )
+    parser.add_argument("--n_epochs", help="How many epochs to perform", default=40, type=int)
+    parser.add_argument("--seed", help="fixed random seed", default=17, type=int)
+    parser.add_argument("--bs", help="batch size", default=4, type=int)
+    args = parser.parse_args()
+
+    random.seed(args.seed)
 
     # paths to training and validation datasets
-    train_path = 'training'
-    val_path = 'validation'
-    test_path = 'test'
+
+    if (args.pre_processing == "none"):
+        train_path = 'training'
+        val_path = 'validation'
+        test_path = 'test'
+    elif (args.pre_processing == "altered_images" or args.pre_processing == "otf"):
+        # ToDo: fix paths with Gygi
+        train_path = 'training'
+        val_path = 'validation'
+        test_path = 'test'
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     # reshape the image to simplify the handling of skip connections and maxpooling
     train_dataset = RoadDataset(train_path, device, augment=True, resize_to=(384, 384))
     val_dataset = RoadDataset(val_path, device, augment=False, resize_to=(384, 384))
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=True)
-    model = UNet().to(device)
-    loss_fn = nn.BCELoss()
-    metric_fns = {'acc': accuracy_fn, 'patch_acc': patch_accuracy_fn}
-    optimizer = torch.optim.Adam(model.parameters())
-    n_epochs = 40
+
+    if (args.model == "cnn"):
+        # ToDo: Implement the cnn preprocessing stuff
+        train_dataset = ImageDataset('training', device)
+        val_dataset = ImageDataset('validation', device)
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.bs, shuffle=True)
+        val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=args.bs, shuffle=True)
+    elif (args.pre_processing == "none" or args.pre_processing == "altered_images"):
+        # ToDo: use not - so good dataloader
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.bs, shuffle=True)
+        val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=args.bs, shuffle=True)
+    elif (args.pre_processing == "otf"):
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.bs, shuffle=True)
+        val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=args.bs, shuffle=True)
+
+    if (args.model == "cnn"):
+        model = PatchCNN().to(device)
+        metric_fns = {'acc': accuracy_fn}
+    elif (args.model == "unet"):
+        model = UNet().to(device)
+        metric_fns = {'acc': accuracy_fn, 'patch_acc': patch_accuracy_fn}
+    elif (args.model == "new_unet"):
+        model = newnet().to(device)
+        metric_fns = {'acc': accuracy_fn, 'patch_acc': patch_accuracy_fn}
+
+    if (args.loss == "BCE"):
+        loss_fn = nn.BCELoss()
+    elif (args.loss == "BCE_with_logit"):
+        loss_fn = nn.BCEWithLogitsLoss()
+    else:
+        print("Plese chose another loss function")
+        return -1
+
+    if (args.optimizer == "adam"):
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    elif (args.optimizer == "SGD"):
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+    elif (args.optimizer == "adamax"):
+        optimizer = torch.optim.Adamax(model.parameters(), lr=args.lr)
+
+    n_epochs = args.n_epochs
 
     try:
         train(train_dataloader, val_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs)
